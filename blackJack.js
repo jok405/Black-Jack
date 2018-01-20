3087875aa40fa71e1fe79cdbd049aaa75f7a24f4
jok405 <john.y.kim84@gmail.com>
2018-01-20 11:04:20 -0500

    initial js setup

---
+++ blackJack.js
@@ -0,0 +1,9 @@
+
+// ==============  blackJack.js | Composed by John Kim | Univeristy of Richmond  ============================= 
+
+//Pseudocode: Math Random to generate card
+// Card 1 will have value = 1-10 or Card 1 = A then it's = 1 or 11.
+// Card 1 is face card = 10
+// Card 2 adds to card 1: if card 1 + card 2 = 21, then game is won.
+// Card 1 + card 2 < 21 offer option to hit
+// 
